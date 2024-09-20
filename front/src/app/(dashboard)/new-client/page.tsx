ce08acc7277f8ddc3a6a596f211ca2f6ff34a4cb
belterrwan <ramirezibarra95@hotmail.com>
2024-09-20 03:24:06 -0600

    add pages

---
+++ front/src/app/(dashboard)/new-client/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewClientPage = () => {
+  return (
+    <div>NewClientPage</div>
+  )
+}
+
+export default NewClientPage
--- front/src/components/globals/Sidebar.tsx
+++ front/src/components/globals/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { LayoutDashboard, FileText, Users, LogOut, UserPlus } from "lucide-react"
+import { LayoutDashboard, FileText, Users, LogOut, UserPlus, Activity } from "lucide-react"
 import Link from 'next/link'
 import React from 'react'
 import { Button } from "@/components/ui/button"
@@ -8,9 +8,9 @@ const Sidebar = () => {
   return (
 <div className="flex h-full flex-col gap-2 ">
       <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-        <Link href="#" className="flex items-center gap-2 font-semibold">
-          <LayoutDashboard className="h-6 w-6" />
-          <span className="">Acme Inc</span>
+        <Link href="/home" className="flex items-center gap-2 font-semibold">
+          <Activity className="h-6 w-6" />
+          <span className="">AI Care</span>
         </Link>
       </div>
       <ScrollArea className="flex-1 px-2 py-2">
@@ -22,26 +22,27 @@ const Sidebar = () => {
             <LayoutDashboard className="h-4 w-4" />
             Home
           </Link>
+
           <Link
-            href="#"
-            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900"
-          >
-            <FileText className="h-4 w-4" />
-            Documents
-          </Link>
-          <Link
-            href="#"
+            href="/clients"
             className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900"
           >
             <Users className="h-4 w-4" />
             Clients
           </Link>
           <Link
-            href="#"
+            href="/new-client"
             className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900"
           >
             <UserPlus className="h-4 w-4" />
             New Client
+          </Link>
+          <Link
+            href="#"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900"
+          >
+            <FileText className="h-4 w-4" />
+            Documents
           </Link>
         </nav>
       </ScrollArea>
