00bc551b9ec75f37d1858bc867fcdd0c58c0a9b7
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2024-09-20 09:47:12 -0600

    RegisterClient

--- front/src/components/regis/FormClients.tsx
+++ front/src/components/regis/FormClients.tsx
@@ -7,30 +7,52 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import Image from 'next/image'
-
-
+import { Label } from "@/components/ui/label"
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 
 const FormClients = () => {
     return (
-        <Card className='flex flex-col'>
-            <Image
-                src="/profile.png"
-                width={500}
-                height={500}
-                alt="Picture of the author"
-            />
+        <Card>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle>Register client</CardTitle>
+                <CardDescription>
+                    Register your client
+                </CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
+
+            <CardContent className="space-y-2">
+                <div className="space-y-1">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" placeholder="Name" />
+                </div>
+                <div className='md:flex flex-row md:space-x-5 '>
+                    <div className=" md:w-1/2">
+                        <Label htmlFor="name">Phone Number</Label>
+                        <Input id="username" placeholder="Phone number" />
+                    </div>
+                    <div className=" md:w-1/2">
+                        <Label htmlFor="name">Birthday</Label>
+                        <Input id="birthday" placeholder="Birthday" />
+                    </div>
+                </div>
+                <div className='md:flex flex-row md:space-x-5'>
+                    <div className="md:w-1/2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" placeholder='your@email.com' />
+                    </div>
+                    <div className="md:w-1/2">
+                        <Label htmlFor="username">Postal code</Label>
+                        <Input id="postal" placeholder="34225" />
+                    </div>
+                </div>
+
             </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardFooter className='justify-end'>
+                <Button>Save changes</Button>
             </CardFooter>
         </Card>
+
 
     )
 }
