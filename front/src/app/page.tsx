ed4eae8fb662eaafd00f2ac766788c87aa0b733b
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2024-09-19 23:00:31 -0600

    homepage

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,53 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+import React from 'react'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Button>This is a Button</Button>
-      </main>
+    <div className='flex justify-center items-center min-h-screen'>
+
+            <Card className="mx-auto md:w-[400px]">
+            <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                Enter your email below to login to your account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="grid gap-4">
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                    />
+                </div>
+                <div className="grid gap-2">
+                    <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <Link href="#" className="ml-auto inline-block text-sm underline">
+                        Forgot your password?
+                    </Link>
+                    </div>
+                    <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                    Login
+                </Button>
+                </div>
+                <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link href="/register" className="underline">
+                    Sign up
+                </Link>
+                </div>
+            </CardContent>
+            </Card>
     </div>
   );
 }
